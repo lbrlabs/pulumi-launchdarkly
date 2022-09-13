0fece951f43cf2904363949deafae6cc3f408717
Lee Briggs <lee@leebriggs.co.uk>
2022-09-13 15:12:48 -0700

    fix command dispatch workflow path

--- provider/resources.go
+++ provider/resources.go
@@ -112,10 +112,7 @@ func Provider() tfbridge.ProviderInfo {
 				"@types/mime": "^2.0.0",
 			},
 			PackageName: "@lbrlabs/pulumi-launchdarkly",
-			// See the documentation for tfbridge.OverlayInfo for how to lay out this
-			// section, or refer to the AWS provider. Delete this section if there are
-			// no overlay files.
-			//Overlay: &tfbridge.OverlayInfo{},
+
 		},
 		Python: &tfbridge.PythonInfo{
 			// List any Python dependencies and their version ranges
